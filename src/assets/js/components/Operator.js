d4b6877ab77ecbae1797922aa781c8b9870c0dfb
Ashish Jindal <ashish.j@greyorange.sg>
2015-12-11 17:04:01 +0530

    Put Back changes

--- src/assets/js/components/Operator.js
+++ src/assets/js/components/Operator.js
@@ -1,12 +1,60 @@
-
 var React = require('react');
+var mainstore = require('../stores/mainstore');
 var PutBack = require('./PutBack.react');
+var PutFront = require('./PutFront');
+var PickBack = require('./PutBack');
+var PickFront = require('./PutFront');
+var Header = require('./Header');
+var appConstants = require('../constants/appConstants');
 
+var currentSeat;
+console.log(currentSeat);
+function getState(){
+  return {
+      seatData: mainstore.seatData()
+  }
+}
 var Operator = React.createClass({
+  getInitialState: function(){
+    return getState();
+  },
+  componentWillMount: function(){
+     mainstore.addChangeListener(this.onChange);
+  },
+  componentWillUnmount: function(){
+    mainstore.removeChangeListener(this.onChange);
+  },
+  onChange: function(){ 
+   this.setState(getState());
+   this.checkSeatType();
+  },
+  checkSeatType : function(){
+    switch(this.state.seatData.mode){
+      case appConstants.PUT:
+        if(this.state.seatData.seat_type === appConstants.BACK){
+          currentSeat = <PutBack data={this.state.seatData}/>;
+        }else{
+          currentSeat = <PutFront data={this.state.seatData}/>;
+        }
+        break;
+      case appConstants.PICK:
+        if(this.state.seatData.seat_type === appConstants.BACK){
+          currentSeat = <PickBack data={this.state.seatData} />;
+        }else{
+          currentSeat = <PickFront data={this.state.seatData}/>;
+        }
+        break; 
+      default:
+        return true;   
+    }
+    this.forceUpdate();
+  },
+  render: function(data){ 
     return (
       <div>
-        <PutBack />
+        {currentSeat}
       </div> 
+
     )
   }
 });

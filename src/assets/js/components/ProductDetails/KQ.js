5e26b213f6f78d0bd44e2568a92e6df819fcec41
aatifbandey <aatifbandey2009@gmail.com>
2015-12-16 19:18:06 +0530

    unused function removed

--- src/assets/js/components/ProductDetails/KQ.js
+++ src/assets/js/components/ProductDetails/KQ.js
@@ -28,19 +28,6 @@ var KQ = React.createClass({
       }
     }
   },
-  postRequest: function() {
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
   componentDidMount: function(){
     var x = $("#keyboard").offset();
     console.log("Top: " + x.top + " Left: " + x.left);

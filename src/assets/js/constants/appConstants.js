33f06c05fbf8f58dca96280ac75399866f0b934d
aatifbandey <aatifbandey2009@gmail.com>
2015-12-28 12:31:42 +0530

    introduced config file for IP

--- src/assets/js/constants/appConstants.js
+++ src/assets/js/constants/appConstants.js
@@ -1,6 +1,4 @@
 var appConstants = {
-	WEBSOCKET_IP : "ws://192.168.1.35:8888/ws",
-	INTERFACE_IP : "http://192.168.1.35:5000",
 	WEBSOCKET_CONNECT : "Websocket connection",
 	LIST_SEATS : "LIST_SEATS",
 	LOGIN: "LOGIN",

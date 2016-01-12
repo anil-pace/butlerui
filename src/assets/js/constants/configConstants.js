a977e661370796ff9e6ef5823d58f0b2b219b3f1
Ashish Jindal <ashish.j@greyorange.sg>
2016-01-12 15:54:21 +0530

    ip changes

--- src/assets/js/constants/configConstants.js
+++ src/assets/js/constants/configConstants.js
@@ -1,6 +1,6 @@
 var configConstants = {
-	WEBSOCKET_IP : "ws://192.168.1.95:8888/ws",
-	INTERFACE_IP : "https://192.168.1.95:5000"
+	WEBSOCKET_IP : "ws://localhost:8888/ws",
+	INTERFACE_IP : "https://localhost:5000"
 };
 
 module.exports = configConstants;

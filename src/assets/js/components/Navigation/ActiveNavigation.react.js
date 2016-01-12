5303fb3b56662fef7f36292dc1542aa7f17f03be
Ashish Jindal <ashish.j@greyorange.sg>
2016-01-12 20:24:10 +0530

    changes

--- src/assets/js/components/Navigation/ActiveNavigation.react.js
+++ src/assets/js/components/Navigation/ActiveNavigation.react.js
@@ -2,6 +2,7 @@ var React = require('react');
 
 var ActiveNavigation = React.createClass({
     render: function() {
+        var d = this.props.serverNavData;
         var navMessagesJson = this.props.navMessagesJson;
         var compData = this.props.data;
         var message_args  = this.props.serverNavData.details.slice(0);
@@ -24,8 +25,9 @@ var ActiveNavigation = React.createClass({
 
                         if(navMessagesJson != undefined){
                             message_args.unshift(navMessagesJson[errorCode]);
-                            var header_message = _.apply(null, message_args);
-                            return header_message;
+                            //var header_message = _.apply(null, message_args);
+                            //return header_message;
+                            return d.description;
                         }
                        
                         }
--- src/assets/js/constants/configConstants.js
+++ src/assets/js/constants/configConstants.js
@@ -1,6 +1,6 @@
 var configConstants = {
-	WEBSOCKET_IP : "ws://192.168.2.210:8888/ws",
-	INTERFACE_IP : "https://192.168.2.210:5000"
+	WEBSOCKET_IP : "ws://192.168.3.128:8888/ws",
+	INTERFACE_IP : "https://192.168.3.128:5000"
 };
 
 module.exports = configConstants;

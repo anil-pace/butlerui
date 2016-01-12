81ecf897da3023e861f4ed80846fcd8ce57e3623
Ashish Jindal <ashish.j@greyorange.sg>
2016-01-12 20:49:06 +0530

    tote changes

--- src/assets/js/components/Bins/Bin.react.js
+++ src/assets/js/components/Bins/Bin.react.js
@@ -51,20 +51,6 @@ var Bin = React.createClass({
                 </div>
             );
 
-        else if((this.props.screenId == appConstants.PICK_BACK_SCAN || this.props.screenId == appConstants.PICK_BACK_BIN ) && ((compData["ppsbin_blink_state"] !=undefined && (compData.ppsbin_blink_state == true || compData.ppsbin_blink_state == "true")) ))
-            return (
-                <div className = "bin  selected blink1">
-                     <div className="tote">
-                        <span className="text">TOTE</span>
-                        <span className="glyphicon glyphicon-info-sign info-icon" onClick={this.showModal.bind(this,compData.bin_info,"bin-info")} >
-                        </span>
-                    </div>
-                    <div className ="item-count">{compData.ppsbin_count}</div>
-                    <div className="pptl selected blink" onClick={this.pressPptl.bind(this, compData.ppsbin_id, compData.ppsbin_state)}>{compData.ppsbin_id}</div>
-                </div>
-            );
-
-
         else if((this.props.screenId == appConstants.PICK_BACK_SCAN || this.props.screenId == appConstants.PICK_BACK_BIN ) && ((compData["ppsbin_blue_state"] !=undefined && (compData.ppsbin_blue_state == true || compData.ppsbin_blue_state == "true")) && compData["totes_associated"] !=undefined && (compData.totes_associated == true || compData.totes_associated == "true")))
             return (
                 <div className = "bin selected">
@@ -78,6 +64,17 @@ var Bin = React.createClass({
                 </div>
             );
 
+
+        else if((this.props.screenId == appConstants.PICK_BACK_SCAN || this.props.screenId == appConstants.PICK_BACK_BIN ) && ((compData["ppsbin_blink_state"] !=undefined && (compData.ppsbin_blink_state == true || compData.ppsbin_blink_state == "true")) ))
+            return (
+                <div className = "bin  selected blink1">
+                    <div className ="item-count">{compData.ppsbin_count}</div>
+                    <div className="pptl selected blink" onClick={this.pressPptl.bind(this, compData.ppsbin_id, compData.ppsbin_state)}>{compData.ppsbin_id}</div>
+                </div>
+            );
+
+
+        
 
         else if((this.props.screenId == appConstants.PICK_BACK_SCAN || this.props.screenId == appConstants.PICK_BACK_BIN ) && (compData["ppsbin_blue_state"] !=undefined && (compData.ppsbin_blue_state == true || compData.ppsbin_blue_state == "true")))
             return (

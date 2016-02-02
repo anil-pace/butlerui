b8cafeab78ccc7020a7707ba5ed95309b5ba8bdd
Anil Kumar <Anil Kumar>
2016-02-02 15:44:18 +0530

    updated spinner

--- src/assets/js/components/Spinner/LoaderButler.js
+++ src/assets/js/components/Spinner/LoaderButler.js
@@ -1,26 +1,4 @@
-/*
-
 var React = require('react');
-var SpinnerButler = require('./SpinnerButler');
-
-var LoaderButler = React.createClass({
-	render:function(){
-		return (
-
-			<div className="loader">
-				
-					<SpinnerButler />
-				
-			</div>
-			);
-	}
-});
-
-module.exports = LoaderButler;
-*/
-
-var React = require('react');
-//var LoaderButler = require('./LoaderButler');
 var SpinnerButler = require('./SpinnerButler');
 
 var LoaderButler = React.createClass({

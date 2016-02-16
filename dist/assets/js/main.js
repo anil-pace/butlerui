6d398e2a184abbfdc46b0526e7c95508c764bb9f
Ashish Jindal <ashish.j@greyorange.sg>
2016-02-16 14:04:39 +0530

    changes

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -41174,8 +41174,8 @@ module.exports = appConstants;
 
 },{}],281:[function(require,module,exports){
 var configConstants = {
-	WEBSOCKET_IP : "ws://192.168.1.120:8888/ws",
-	INTERFACE_IP : "https://192.168.1.120:5000"
+	WEBSOCKET_IP : "ws://192.168.2.211:8888/ws",
+	INTERFACE_IP : "https://192.168.2.211:5000"
 };
 
 module.exports = configConstants;

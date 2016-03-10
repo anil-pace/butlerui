a4a93e10f242274db7b29f345fd6a00a69771b78
Ashish Jindal <ashish.j@greyorange.sg>
2016-03-10 16:54:58 +0530

    changes for audit bug

--- src/assets/js/constants/configConstants.js
+++ src/assets/js/constants/configConstants.js
@@ -1,6 +1,6 @@
 var configConstants = {
-	WEBSOCKET_IP : "wss://192.168.3.26/wss",
-	INTERFACE_IP : "https://192.168.3.26"
+	WEBSOCKET_IP : "wss://192.168.1.40/wss",
+	INTERFACE_IP : "https://192.168.1.40"
 };
 
 module.exports = configConstants;

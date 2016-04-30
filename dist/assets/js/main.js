244d529a98695565571665148b542bb67218a71d
Ashish Jindal <ashish.j@greyorange.sg>
2016-04-30 10:45:30 +0530

    ip build changes

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -43246,8 +43246,8 @@ module.exports = appConstants;
 
 },{}],284:[function(require,module,exports){
 var configConstants = {
-	WEBSOCKET_IP : "ws://192.168.2.175:8888/ws",
-	INTERFACE_IP : "https://192.168.2.175:5000"
+	WEBSOCKET_IP : "wss://localhost/wss",
+	INTERFACE_IP : "https://localhost"
 };
 
 module.exports = configConstants;

3627eb8c7d2d6a11e7700abeb0b2bc3ac5dcaff4
arshdeep <arshdeep.m@greyorange.sg>
2016-09-08 16:47:12 +0530

    localhost issue

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -43355,8 +43355,8 @@ module.exports = appConstants;
 
 },{}],284:[function(require,module,exports){
 var configConstants = {
-	WEBSOCKET_IP : "wss://192.168.8.118/wss",
-	INTERFACE_IP : "https://192.168.8.118"
+	WEBSOCKET_IP : "wss://localhost/wss",
+	INTERFACE_IP : "https://localhost"
 };
 module.exports = configConstants;
 

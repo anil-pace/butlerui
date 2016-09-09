e161fe0afc7fab21a9a97fdb2d2023009025b25f
Himanshu Saraswat <himanshu.s@GGN00338.local>
2016-09-09 16:01:54 +0530

    Pick front image issue in japanese UI fixed

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -40158,9 +40158,10 @@ var ProductDetails = React.createClass({displayName: "ProductDetails",
              if(data_locale.locale == locale){
                 if(data_locale.display_name != 'product_local_image_url' ){
                   product_info_locale[data_locale.display_name] = keyValue;
-                }else if(data_locale.display_name == 'product_local_image_url' ){
-                  image_url[data_locale.display_name] = imageKey;
                 }
+              }
+              if(data_locale.display_name == 'product_local_image_url' ){
+                  image_url[data_locale.display_name] = imageKey;
               }
             
             }

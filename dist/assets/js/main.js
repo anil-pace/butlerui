b283a4be442aac9cb2f29909f273f80ea9faf2df
Sourabh Jajoria <sourabh.j@GGN00329.local>
2016-11-04 12:12:13 +0530

    Height issue fixed, tested for rack type 19, configConstants reset

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -43377,8 +43377,8 @@ module.exports = appConstants;
 
 },{}],284:[function(require,module,exports){
 var configConstants = {
-	WEBSOCKET_IP : "wss://192.168.8.183/wss",
-	INTERFACE_IP : "https://192.168.8.183"
+	WEBSOCKET_IP : "wss://localhost/wss",
+	INTERFACE_IP : "https://localhost"
 };
 module.exports = configConstants;
 

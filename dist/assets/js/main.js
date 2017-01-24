2b4c3f629e50b9835c8de4bf4fc9d28de355b88a
Sourabh Jajoria <sourabh.j@greyorange.sg>
2017-01-24 16:46:35 +0530

    ConfigConstants restored

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -44682,8 +44682,8 @@ module.exports = appConstants;
 
 },{}],294:[function(require,module,exports){
 var configConstants = {
-	WEBSOCKET_IP : "ws://192.168.3.115:8888/ws",
-	INTERFACE_IP : "https://192.168.3.115:5000"
+	WEBSOCKET_IP : "wss://localhost/wss",
+	INTERFACE_IP : "https://localhost"
 };
 module.exports = configConstants;
 

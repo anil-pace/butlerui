b7f32b67e24cba20befefd3a3bc1159c3a53e900
Himanshu Saraswat <himanshu.s@GGN000158.local>
2017-02-16 14:06:54 +0530

    action button added on press pptl

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -45607,8 +45607,8 @@ module.exports = appConstants;
 
 },{}],298:[function(require,module,exports){
 var configConstants = {
-	WEBSOCKET_IP : "ws://192.168.3.136:8888/ws",
-	INTERFACE_IP : "http://192.168.3.136:5000"
+	WEBSOCKET_IP : "wss://localhost/wss",
+	INTERFACE_IP : "https://localhost"
 };
 module.exports = configConstants;
 

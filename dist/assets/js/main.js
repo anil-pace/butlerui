10280cb9bc51989c0205ded6b6e7ce40ad7e6748
himanshu.s <himanshu.s@greyorange.sg>
2017-03-15 17:01:11 +0530

    merging with audit branch

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -45628,8 +45628,8 @@ module.exports = appConstants;
 
 },{}],298:[function(require,module,exports){
 var configConstants = {
-	WEBSOCKET_IP : "ws://192.168.12.118:8888/ws",
-	INTERFACE_IP : "http://192.168.12.118:5000"
+	WEBSOCKET_IP : "wss://localhost/wss",
+	INTERFACE_IP : "https://localhost"
 };
 module.exports = configConstants;
 

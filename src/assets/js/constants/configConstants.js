661ad58a148771d7869050ab5af49344a5ea6eb2
himanshu.s <himanshu.s@greyorange.sg>
2017-03-29 18:28:47 +0530

    added localhost to config constants

--- src/assets/js/constants/configConstants.js
+++ src/assets/js/constants/configConstants.js
@@ -1,5 +1,5 @@
 var configConstants = {
-	WEBSOCKET_IP : "ws://192.168.13.160:8888/ws",
-	INTERFACE_IP : "http://192.168.13.160:5000"
+	WEBSOCKET_IP : "wss://localhost/wss",
+	INTERFACE_IP : "https://localhost"
 };
 module.exports = configConstants;

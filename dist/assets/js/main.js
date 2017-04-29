63b40bd815a0b5ca0c524d9eee0bad37052a1567
Raja D <raja.d@greyorange.sg>
2017-04-29 08:15:19 +0530

    CSS fixed

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -45941,8 +45941,8 @@ module.exports = appConstants;
 
 },{}],298:[function(require,module,exports){
 var configConstants = {
-	WEBSOCKET_IP : "ws://192.168.13.85:8888/ws",
-	INTERFACE_IP : "https://192.168.13.85:5000"
+	WEBSOCKET_IP : "wss://localhost/wss",
+	INTERFACE_IP : "https://localhost"
 };
 module.exports = configConstants;
 
--- src/assets/js/constants/configConstants.js
+++ src/assets/js/constants/configConstants.js
@@ -1,5 +1,5 @@
 var configConstants = {
-	WEBSOCKET_IP : "ws://192.168.13.85:8888/ws",
-	INTERFACE_IP : "https://192.168.13.85:5000"
+	WEBSOCKET_IP : "wss://localhost/wss",
+	INTERFACE_IP : "https://localhost"
 };
 module.exports = configConstants;

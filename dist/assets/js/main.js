4e1bf59fd0fda9ed3d90370b34ea5834c349a1a2
Raja D <raja.d@greyorange.sg>
2017-07-07 14:27:52 +0530

    Changes for Put Front https://work.greyorange.com/jira/browse/BSS-3078

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -46788,11 +46788,8 @@ module.exports = appConstants;
 
 },{}],299:[function(require,module,exports){
 var configConstants = {
-	WEBSOCKET_IP : "ws://192.168.3.106:8888/ws",
-	INTERFACE_IP : "https://192.168.3.106:5000"
-	
-	// WEBSOCKET_IP : "wss://localhost/wss",
-	// INTERFACE_IP : "https://localhost"
+	WEBSOCKET_IP : "wss://localhost/wss",
+	INTERFACE_IP : "https://localhost"
 };
 module.exports = configConstants;
 

68d6aa44ea06e2da5529c12ffadbaf87cf8bed24
Raja D <raja.d@greyorange.sg>
2017-09-13 21:27:36 +0530

    Added message for waiting for MPU screen

--- src/assets/js/constants/configConstants.js
+++ src/assets/js/constants/configConstants.js
@@ -1,7 +1,7 @@
 var configConstants = {
-// WEBSOCKET_IP : "ws://192.168.3.66:8888/ws",
-// 	INTERFACE_IP : "http://192.168.3.66:5000"
-WEBSOCKET_IP : "wss://192.168.8.83/wss",
-	INTERFACE_IP : "https://192.168.8.83"
+WEBSOCKET_IP : "ws://192.168.3.66:8888/ws",
+	INTERFACE_IP : "http://192.168.3.66:5000"
+// WEBSOCKET_IP : "wss://192.168.8.83/wss",
+// 	INTERFACE_IP : "https://192.168.8.83"
 };
 module.exports = configConstants;

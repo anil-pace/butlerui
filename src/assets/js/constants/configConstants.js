ff4f3154784bfe6506ce07ff44d43a5f8b25a820
Raja D <raja.d@greyorange.sg>
2017-09-14 16:06:28 +0530

    Remove mock data https://work.greyorange.com/jira/browse/BSS-4588

--- src/assets/js/constants/configConstants.js
+++ src/assets/js/constants/configConstants.js
@@ -1,7 +1,5 @@
 var configConstants = {
-// WEBSOCKET_IP : "ws://192.168.3.66:8888/ws",
-// 	INTERFACE_IP : "http://192.168.3.66:5000"
-WEBSOCKET_IP : "wss://192.168.8.83/wss",
-	INTERFACE_IP : "https://192.168.8.83"
+WEBSOCKET_IP : "wss://localhost/wss",
+	INTERFACE_IP : "https://localhost"
 };
 module.exports = configConstants;

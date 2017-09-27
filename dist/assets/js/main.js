17c9831d0594d2c0a57a75ac27c25b5e35fc6fe9
Raja D <raja.d@greyorange.sg>
2017-09-27 12:44:18 +0530

    CSS changes

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -47756,10 +47756,10 @@ module.exports = appConstants;
 
 },{}],301:[function(require,module,exports){
 var configConstants = {
-WEBSOCKET_IP : "wss://192.168.8.83/wss",
-	INTERFACE_IP : "https://192.168.8.83"
-// WEBSOCKET_IP : "ws://192.168.3.106:8888/ws",
-// 	INTERFACE_IP : "https://192.168.3.106:5000"
+// WEBSOCKET_IP : "wss://192.168.8.83/wss",
+// 	INTERFACE_IP : "https://192.168.8.83"
+WEBSOCKET_IP : "ws://192.168.3.106:8888/ws",
+	INTERFACE_IP : "https://192.168.3.106:5000"
 };
 module.exports = configConstants;
 

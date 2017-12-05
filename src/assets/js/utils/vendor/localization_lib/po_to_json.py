ac00f7985e264a17664901bd681119b9ea343991
arshdeep <arshdeep.m@greyorange.sg>
2017-12-05 20:19:04 +0530

    Updated Japanese Translations from rc/sprint-37 Updated the po_to_json.py to check for duplicate entries in the target files

--- src/assets/js/utils/vendor/localization_lib/po_to_json.py
+++ src/assets/js/utils/vendor/localization_lib/po_to_json.py
@@ -31,14 +31,28 @@ for srcfile in args:
 	xlate_map = {}
 	
 	po = polib.pofile(srcfile, autodetect_encoding=False, encoding="utf-8", wrapwidth=-1)
+	po2 = polib.pofile(srcfile, autodetect_encoding=False, encoding="utf-8", wrapwidth=-1)
+	duplicateEntries = [];
 	for entry in po:
 		if entry.obsolete  or entry.msgstr == entry.msgid:
-			continue
-			
+			continue;
+		count = 0;
+		for entry1 in po2:			
+			if entry1.msgid == entry.msgid:
+				count = count + 1;
+				if count > 1:
+					count = 0;
+					print("Duplicate found %s" % entry1.msgid)
+					duplicateEntries.append(entry.msgid);
 		xlate_map[entry.msgid] = entry.msgstr;
-		print("converting: %s" % (entry.msgid))		
+	if len(duplicateEntries)> 1:
+		for entry in duplicateEntries:			
+			print("Total number of duplicate entries %d" %len(duplicateEntries));
+			exit();
+
 	dest = open(destfile, "w")
-	
+	print("No duplicate entries found");
+	print("Writing to the JSON now");
 	dest.write(json.dumps(xlate_map, sort_keys = True));
 	
 	dest.close()

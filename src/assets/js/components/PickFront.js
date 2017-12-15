300d4c57dd33948a695cc571f8fdfd74cb7386f1
Anil Kumar <anil.k@GGN000046.local>
2017-12-15 18:46:00 +0530

    cherrypicking + conflict resolve

--- src/assets/js/components/PickFront.js
+++ src/assets/js/components/PickFront.js
@@ -61,9 +61,9 @@ var PickFront = React.createClass({
         }
     },
     getNotificationComponent: function () {
-        if (this.state.PickFrontNotification != undefined)
-            this._notification = <Notification notification={this.state.PickFrontNotification}
-                                               navMessagesJson={this.props.navMessagesJson}/>
+        if (this.state.PickFrontNotification != undefined){
+            this._notification = <Notification notification={this.state.PickFrontNotification} navMessagesJson={this.props.navMessagesJson}/>
+        }
         else{
             if($(".modal.notification-error").is(":visible")){
                 setTimeout((function(){
@@ -74,6 +74,19 @@ var PickFront = React.createClass({
 
                 }),0)
 
+                return null
+            }
+            else if($(".modal.in").is(":visible")){
+                setTimeout((function(){
+                  if($('.modal.in').find("div").hasClass("modal-footer")){
+                      //check when errorcode is true and modal has buttons
+                      $('.modal.in').data('bs.modal').options.backdrop='static';
+                  }
+                  else{
+                      //check when errorcode is true and modal has NO buttons
+                      $('.modal.in').data('bs.modal').options.backdrop=true;
+                  }
+              }),0)
                 return null
             }
             this._notification = "";

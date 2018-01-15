7af0eea9edff825c0df02a242cc429a91d6eaa75
Anil Kumar <anil.k@GGN000046.local>
2018-01-15 23:34:17 +0530

    removing unwanted codes

--- src/assets/js/components/Rack/MsuRackFlex.js
+++ src/assets/js/components/Rack/MsuRackFlex.js
@@ -1,159 +1,11 @@
 var React = require('react');
 
-
-// var xyz = [
-//   {
-//   "rack_type_rec":[
-//     {
-//     "height": "33",
-//     "orig_coordinates": [
-//       0,
-//       5
-//     ],
-//     "length": "32",
-//     "barcodes":["A.01", "A.02"]
-//   },
-//   {
-//     "height": "33",
-//     "orig_coordinates": [
-//       32,
-//       5
-//     ],
-//     "length": "32",
-//     "barcodes":["B.01", "B.02"]
-//   },
-//   {
-//     "height": "33",
-//     "orig_coordinates": [
-//       64,
-//       5
-//     ],
-//     "length": "32",
-//     "barcodes":["C.01", "C.02"]
-//   },
-//   {
-//     "height": "33",
-//     "orig_coordinates": [
-//       0,
-//       43
-//     ],
-//     "length": "32",
-//     "barcodes":["D.01", "D.02"]
-//   },
-//   {
-//     "height": "33",
-//     "orig_coordinates": [
-//       32,
-//       43
-//     ],
-//     "length": "32",
-//     "barcodes":["E.01", "E.02"]
-//   },
-//   {
-//     "height": "33",
-//     "orig_coordinates": [
-//       64,
-//       43
-//     ],
-//     "length": "32",
-//     "barcodes":["F.01", "F.02"]
-//   },
-//   {
-//     "height": "33",
-//     "orig_coordinates": [
-//       0,
-//       81
-//     ],
-//     "length": "32",
-//     "barcodes":["G.01", "G.02"]
-//   },
-//   {
-//     "height": "33",
-//     "orig_coordinates": [
-//       32,
-//       81
-//     ],
-//     "length": "32",
-//     "barcodes":["H.01", "H.02"]
-//   },
-//   {
-//     "height": "33",
-//     "orig_coordinates": [
-//       64,
-//       81
-//     ],
-//     "length": "32",
-//     "barcodes":["I.01", "I.02"]
-//   },
-//   {
-//     "height": "33",
-//     "orig_coordinates": [
-//       0,
-//       119
-//     ],
-//     "length": "32",
-//     "barcodes":["J.01", "J.02"]
-//   },
-//   {
-//     "height": "33",
-//     "orig_coordinates": [
-//       32,
-//       119
-//     ],
-//     "length": "32",
-//     "barcodes":["K.01", "K.02"]
-//   },
-//   {
-//     "height": "33",
-//     "orig_coordinates": [
-//       64,
-//       119
-//     ],
-//     "length": "32",
-//     "barcodes":["L.01", "L.02"]
-//   },
-//   {
-//     "height": "33",
-//     "orig_coordinates": [
-//       0,
-//       157
-//     ],
-//     "length": "32",
-//     "barcodes":["M.01", "M.02"]
-//   },
-//   {
-//     "height": "33",
-//     "orig_coordinates": [
-//       32,
-//       157
-//     ],
-//     "length": "32",
-//     "barcodes":["N.01", "N.02", "N.03"]
-//   },
-//   {
-//     "height": "33",
-//     "orig_coordinates": [
-//       64,
-//       157
-//     ],
-//     "length": "32",
-//     "barcodes":["O.01", "O.02"]
-//   }
-
-//     ]
-//   },
-//   {"slot_barcodes":["003.1.N.01", "003.1.N.02", "003.1.N.03"]}
-// ];
-
-
 var MsuRackFlex = React.createClass({
 
     getInitialState: function(){
-        //return this._sortSlots(xyz[0].rack_type_rec);
         return this._sortSlots(this.props.rackDetails);
     },
     componentWillReceiveProps: function() {
-        //this._sortSlots(xyz[0].rack_type_rec);
         this._sortSlots(this.props.rackDetails);
     },
 
@@ -165,13 +17,13 @@ var MsuRackFlex = React.createClass({
          */
 
          var lines = document.getElementsByClassName("connectingLine");
-         if(lines.length==0){
-          var strEl = document.querySelectorAll("#selectedSlot")[0];
-          strEl = strEl ? strEl.parentNode : null;
-          var endEl  = document.querySelectorAll("#slotDisplayArea")[0];
-          if(strEl && endEl){
-           this.connect(strEl, endEl, "#6d6d6d", 3,"drawerLine");
-          }
+         if(lines.length===0){
+            var strEl = document.querySelectorAll("#selectedSlot")[0];
+            strEl = strEl ? strEl.parentNode : null;
+            var endEl  = document.querySelectorAll("#slotDisplayArea")[0];
+            if(strEl && endEl){
+             this.connect(strEl, endEl, "#6d6d6d", 3);
+            }
         }
     },
 
@@ -194,18 +46,10 @@ var MsuRackFlex = React.createClass({
       var selectedSlotIndex;
 
 
-      var newBarcodes = []; // for storing post truncation data
+      var newBarcodes = []; // for storing post data manipulation
       var selectedSlotIds = "";
 
-      //var y = xyz[1].slot_barcodes.map(function(slotBarcodes,idx){
-        //{"slot_barcodes":["003.1.N.01", "003.1.N.02", "003.1.N.03"]}
-        var y = this.props.slotBarcodes.map(function(slotBarcodes,idx){
-        // var barcodesSplit = slotBarcodes.split(".");
-        // var barcodeId = barcodesSplit[2]+"."+barcodesSplit[3];
-
-        // console.log("===================================>");
-        // console.log("slotsToHighlight post Truncation " + barcodeId);
-
+      this.props.slotBarcodes.map(function(slotBarcodes,idx){
           var str = slotBarcodes,
           delimiter = '.',
           start = 2,
@@ -213,27 +57,19 @@ var MsuRackFlex = React.createClass({
           if(tokens.length > 1) result = tokens.join("."); //take extra care when we have 3rd "." as delimiter
           else result = tokens;
 
-          // "qwe.fs.xczv.xcv.xcv.x".replace(/([^\.]*\.){3}/, '');
-          // "xcv.xcv.x"
-
           newBarcodes.push(result);
-        });
-
-      console.log(newBarcodes);
-      //selectedSlotIds =newBarcodes[0].replace(".",'')+" - "+newBarcodes[newBarcodes.length-1].replace(".",'');
-        selectedSlotIds = newBarcodes;
+      });
+      selectedSlotIds = newBarcodes;
       
 
       vSlots.map(function(eachSlot, index){
-         var eachSlotBarcodes = eachSlot.barcodes;
-         if(!eachSlotBarcodes) return;
-          console.log("eachSlotBarcodes" + eachSlotBarcodes);
-          if(eachSlotBarcodes.length === newBarcodes.length){
-              console.log("length of both the arrays is same");
-              if( JSON.stringify(newBarcodes)==JSON.stringify(eachSlotBarcodes) ){
-                 selectedSlotIndex = index;
-              }
+        var eachSlotBarcodes = eachSlot.barcodes;
+        if(!eachSlotBarcodes) return;
+        if(eachSlotBarcodes.length === newBarcodes.length){
+          if( JSON.stringify(newBarcodes)==JSON.stringify(eachSlotBarcodes) ){
+             selectedSlotIndex = index;
           }
+        }
       });
       
       lastHSlot = vSlots.reduce(function(prevSlot,currSlot){
@@ -289,13 +125,13 @@ var MsuRackFlex = React.createClass({
     },
 
     getOffset( el ) {
-        var rect = el.getBoundingClientRect();
-        return {
-            left: rect.left + window.pageXOffset,
-            top: rect.top + window.pageYOffset,
-            width: rect.width || el.offsetWidth,
-            height: rect.height || el.offsetHeight
-        };
+      var rect = el.getBoundingClientRect();
+      return {
+          left: rect.left + window.pageXOffset,
+          top: rect.top + window.pageYOffset,
+          width: rect.width || el.offsetWidth,
+          height: rect.height || el.offsetHeight
+      };
     },
     _createSlotLayouts: function(vSlots, lastHSlot, lastVSlot, selectedSlotIndex, selectedSlotIds) {
         if ((vSlots.constructor !== Array && vSlots.length < 1) || !(lastHSlot.length) || !(lastVSlot.length)){
@@ -314,9 +150,6 @@ var MsuRackFlex = React.createClass({
          var totalRackWidth = Number(lastHSlot.orig_coordinates[0]) + Number(lastHSlot.length);
          var totalRackHeight = Number(lastVSlot.orig_coordinates[1]) + Number(lastVSlot.height);
 
-         console.log("TotalPPSWidth is" + totalRackWidth);
-         console.log("totalRackHeight is" + totalRackHeight);
-
          for (var i =0; i<vSlots.length ;i++){
                 var binWidth = vSlots[i].length * horFactor+'%';
                 var binHeight = vSlots[i].height * vertFactor +'%';
@@ -331,10 +164,6 @@ var MsuRackFlex = React.createClass({
                 var vRackHeight = totalRackHeight * vertFactor; //rack height in ratio
 
                 let sum= Number(ibottom.substring(0,ibottom.length-1)) + Number(binHeight.substring(0,binHeight.length-1));
-
-                console.log("========================>");
-                console.log("ibottom + binHeight " + sum);
-
 
                 /* Check for BORDER of bins-flex - START*/
 
@@ -358,60 +187,21 @@ var MsuRackFlex = React.createClass({
                   var setSlotBackground="white";
 
                 vHTMLSlots.push(
-                                   <div className="subSlot"
-                                      style={{
-                                        width: binWidth,
-                                        height: binHeight,
-                                        bottom: ibottom,
-                                        left:ileft,
-                                        borderTop: borderTop,
-                                        borderRight: borderRight,
-                                        borderBottom: "1px solid #939598",
-                                        borderLeft: borderLeft,
-                                        background: setSlotBackground
-                                      }}>{drawALine}
-                                   </div>
-                                   )
+                               <div className="subSlot"
+                                  style={{
+                                    width: binWidth,
+                                    height: binHeight,
+                                    bottom: ibottom,
+                                    left:ileft,
+                                    borderTop: borderTop,
+                                    borderRight: borderRight,
+                                    borderBottom: "1px solid #939598",
+                                    borderLeft: borderLeft,
+                                    background: setSlotBackground
+                                  }}>{drawALine}
+                               </div>
+                              )
 
-                // if(i===selectedSlotIndex){
-                //   vHTMLSlots.push(
-                //                    <div className="subSlot"
-                //                       style={{
-                //                         width: binWidth,
-                //                         height: binHeight,
-                //                         bottom: ibottom,
-                //                         left:ileft,
-                //                         borderTop: borderTop,
-                //                         borderRight: borderRight,
-                //                         borderBottom: "1px solid #939598",
-                //                         borderLeft: borderLeft,
-                //                         background: "#939598",
-                //                         color:"white",
-                //                         fontSize: "1.5em",
-                //                         textAlign: "center",
-                //                         fontWeight: "bold"
-                //                       }}>
-                //                       <div id="selectedSlot"></div>
-                //                    </div>
-                //                    )
-                // }
-                // else{
-                //   vHTMLSlots.push(
-                //                    <div className="subSlot"
-                //                       style={{
-                //                         width: binWidth,
-                //                         height: binHeight,
-                //                         bottom: ibottom,
-                //                         left:ileft,
-                //                         borderTop: borderTop,
-                //                         borderRight: borderRight,
-                //                         borderBottom: "1px solid #939598",
-                //                         borderLeft: borderLeft,
-                //                         background: "white"
-                //                       }}>
-                //                    </div>
-                //                    )
-                // }
               }
               //attach legs to Rack
               vHTMLSlots.push(<div className="legsSpaceContainer"> </div>);
@@ -420,14 +210,6 @@ var MsuRackFlex = React.createClass({
 
     render: function() {
 
-      console.log("============================>");
-      console.log("document width is" + document.body.clientWidth);
-      console.log("document height is" + document.body.clientHeight);
-      console.log("lastHSlot" + this.state.lastHSlot.orig_coordinates[0]);
-      console.log("lastVSlot" + this.state.lastVSlot.orig_coordinates[1]);
-
-
-      
       var vHTMLSlots = this._createSlotLayouts(this.state.vSlots,
                                                this.state.lastHSlot,
                                                this.state.lastVSlot,

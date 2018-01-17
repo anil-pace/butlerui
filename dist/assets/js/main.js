becf496e47b6f81da14b2b5fd4c4663562e85373
Raja D <raja.d@greyorange.sg>
2018-01-17 16:12:55 +0530

    Removed IP https://work.greyorange.com/jira/browse/BSS-6405

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -47858,8 +47858,8 @@ module.exports = appConstants;
 
 },{}],300:[function(require,module,exports){
 var configConstants = {
-WEBSOCKET_IP : "wss://192.168.8.101/wss",
- INTERFACE_IP : "https://192.168.8.101"
+WEBSOCKET_IP : "wss://localhost/wss",
+ INTERFACE_IP : "https://localhost"
 };
 module.exports = configConstants;
 

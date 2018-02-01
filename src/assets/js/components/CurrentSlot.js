eb68da19811230d87ca52936e2ade20e1e6dae41
Raja D <raja.d@greyorange.sg>
2018-02-01 10:23:00 +0530

    https://work.greyorange.com/jira/browse/BSS-6599 Fixed in 47

--- src/assets/js/components/CurrentSlot.js
+++ src/assets/js/components/CurrentSlot.js
@@ -3,14 +3,22 @@ var Header = require('./Header');
 var allresourceConstants = require('../constants/resourceConstants');
 
 var CurrentSlot = React.createClass({
-	render:function(){		
-		var slotStart = this.props.slotDetails[0].split(".")[2] + this.props.slotDetails[0].split(".")[3].slice(1, 2);
-		var slotEnd = this.props.slotDetails[1].split(".")[2] + this.props.slotDetails[1].split(".")[3].slice(1, 2);
-		var range = slotStart + " - " + slotEnd;		
-		
+	render:function(){	
+		var range='';
+		var slotArr=this.props.slotDetails ||[];
+		finalArray=[],finalString='';
+		if(slotArr.length){
+			slotArr.forEach( function (arrayItem)
+				{
+					var arrdata=arrayItem.split('.');
+					var splitData=arrdata[arrdata.length-2]+'.'+arrdata[arrdata.length-1];
+					finalArray.push(splitData);
+				});
+		range=finalArray.join(',');
+		}		
 		return (
 				<div className="currentSlotWrapper">
-					<div className="slotRange">	{range} </div>
+					<div className="slotRange">{range}</div>
 					<div className="slotFooter"> {_(allresourceConstants.CURR_SLOT)} </div>
 				</div>
 						

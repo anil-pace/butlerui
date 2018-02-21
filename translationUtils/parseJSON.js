64a19bf9d4f8177d8e8e1de1fce46452f5ede126
himanshu.s <himanshu.s@greyorange.sg>
2018-02-21 14:48:59 +0530

    testing jenkins script

---
+++ translationUtils/parseJSON.js
@@ -0,0 +1,16 @@
+
+function parseJSON(json){
+	let parsedJSON={};
+	for(let k in json){
+		if(k !== ""){
+			let msgId = json[k][msgid],
+			msgStr = json[k][msgstr][0];
+			parsedJSON[msgId] = msgStr;
+		}
+	}
+	return parsedJSON
+}
+
+module.exports={
+	parseJSON:parseJSON
+}

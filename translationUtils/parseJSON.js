4861ffd775336df038975616d7a95a2c8f2f804d
himanshu.s <himanshu.s@greyorange.sg>
2018-03-08 11:27:51 +0530

    adding charset to utf8

--- translationUtils/parseJSON.js
+++ translationUtils/parseJSON.js
@@ -10,6 +10,19 @@ function parseJSON(json){
 	return parsedJSON
 }
 
+/*function toUnicode(theString) {
+  var unicodeString = '';
+  for (var i=0; i < theString.length; i++) {
+    var theUnicode = theString.charCodeAt(i).toString(16).toUpperCase();
+    while (theUnicode.length < 4) {
+      theUnicode = '0' + theUnicode;
+    }
+    theUnicode = '\\u' + theUnicode;
+    unicodeString += theUnicode;
+  }
+  return unicodeString;
+}*/
+
 module.exports={
 	parseJSON:parseJSON
 }

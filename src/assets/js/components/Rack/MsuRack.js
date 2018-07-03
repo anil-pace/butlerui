5c09e0096092313abb821d00ce49e940419bf8e0
Himanshu Saraswat <himanshu.s@greyorange.sg>
2018-07-03 12:56:23 +0000

    BSS-8323 UI changes for User Directed Put away

--- src/assets/js/components/Rack/MsuRack.js
+++ src/assets/js/components/Rack/MsuRack.js
@@ -5,7 +5,7 @@ var MsuRackFlex = require('./MsuRackFlex');
 
 var MsuRack = React.createClass({
 
-	render: function(){
+    render: function(){
 		return (
 				<div className="drawRackWrapper">
                     <MsuRackFlex hideSlotDetails={this.props.hideSlotDetails} rackDetails={this.props.rackData.rack_type_rec} slotBarcodes={this.props.rackData.slot_barcodes} rackWidth={this.props.rackData.rack_width} putDirectionFlex={this.props.putDirection} />

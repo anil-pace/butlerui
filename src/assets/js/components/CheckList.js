066ff55a9a0fa25256dfcb8d3daa035d61cdc2ca
Anil Kumar <anil.k@GGN000046.local>
2018-08-03 11:25:32 +0530

    updated with review comments

--- src/assets/js/components/CheckList.js
+++ src/assets/js/components/CheckList.js
@@ -7,26 +7,28 @@ var CheckList = React.createClass({
         var checklistIndex = this.props.checklistIndex;
         var eachRow = [];
         var dataToDisplay = ""; var iconToDisplay = ""; var applyClass = "boldText"; 
-        checklistData.map(function(key, value) {
-            if(key === parseInt(checklistIndex)){ /* checklistData[key]["action_results"] === null */
-                iconToDisplay = <img className="img-responsive" src="assets/images/current_checklist.png"/>;
-            }
-            else if(key < parseInt(checklistIndex)){
-                dataToDisplay = ": " + checklistData[key]["action_results"]["value"];
-                iconToDisplay = <img className="img-responsive" src="assets/images/done_checklist.png"/>;
-            }
-            else{
-                iconToDisplay = <img className="img-responsive" src="assets/images/toBeDone_checklist.png"/>;
-                applyClass = "greyText";
-            }
-            eachRow.push(
-                <tr>
-                    <td className={applyClass}> {checklistData[key]["action_parameters"]["key"] + dataToDisplay} </td>
-                    <td className="value"> {iconToDisplay} </td>
-                </tr>
-            );
-        });
-        return eachRow;
+        if(checklistData && checklistIndex){
+            checklistData.map(function(key, value) {
+                if(key === parseInt(checklistIndex, 10)){ /* checklistData[key]["action_results"] === null */
+                    iconToDisplay = <img className="img-responsive" src="assets/images/current_checklist.png"/>;
+                }
+                else if(key < parseInt(checklistIndex,10)){
+                    dataToDisplay = ": " + checklistData[key]["action_results"]["value"];
+                    iconToDisplay = <img className="img-responsive" src="assets/images/done_checklist.png"/>;
+                }
+                else{
+                    iconToDisplay = <img className="img-responsive" src="assets/images/toBeDone_checklist.png"/>;
+                    applyClass = "greyText";
+                }
+                eachRow.push(
+                    <tr>
+                        <td className={applyClass}> {checklistData[key]["action_parameters"]["key"] + dataToDisplay} </td>
+                        <td className="value"> {iconToDisplay} </td>
+                    </tr>
+                );
+            });
+            return eachRow;
+        }
     },
 
     render: function() {

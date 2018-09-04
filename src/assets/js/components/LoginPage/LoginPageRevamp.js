0067fc5371d37431ef6f7556b75a5171105e54f5
Raja Dey <raja.d@greyorange.sg>
2018-09-04 18:29:52 +0530

    html css

---
+++ src/assets/js/components/LoginPage/LoginPageRevamp.js
@@ -0,0 +1,182 @@
+var React = require('react');
+var LinkedStateMixin = require('react-addons-linked-state-mixin');
+var Router = require('react-router');
+var mainstore = require('../../stores/mainstore');
+var loginstore = require('../../stores/loginstore');
+var CommonActions = require('../../actions/CommonActions');
+var Operator = require('../Operator');
+var allSvgConstants = require('../../constants/svgConstants');
+var resourceConstants = require('../../constants/resourceConstants');
+var utils = require('../../utils/utils.js');
+var appConstants=require('../../constants/appConstants');
+
+var virtualKeyBoard_login, _seat_name = null;
+function getState(){
+   return {
+      flag: loginstore.getFlag(),
+      seatList : loginstore.seatList(),
+      username : '',
+      password : '',
+      showError: loginstore.getErrorMessage(),
+      getLang : loginstore.getLang(),
+      getCurrentLang : loginstore.getCurrentLang()
+  }
+}
+
+var LoginPageRevamp = React.createClass({
+ mixins:[LinkedStateMixin],
+  getInitialState: function(){
+    return getState();
+  },
+  handleLogin: function(e){ 
+  if(_seat_name == null){
+    _seat_name = this.refs.seat_name.value;
+  }
+    var data = {
+        'data_type': 'auth',
+        'data': {
+              'username': this.refs.username.value,
+              'password': this.refs.password.value,
+              'seat_name': _seat_name
+              
+          }
+      }
+    console.log(data);
+    utils.generateSessionId();
+    CommonActions.login(data);
+    //CommonActions.clearNotification(); Removing this clear notification to enable pop-up modal after login 
+  }, 
+  componentDidMount: function(){
+    mainstore.addChangeListener(this.onChange);
+    loginstore.addChangeListener(this.onChange);
+    CommonActions.webSocketConnection(); 
+    CommonActions.listSeats();
+    CommonActions.setLanguage();                 //Dispatch setLanguage action
+    if(this.state.getLang){
+      CommonActions.changeLanguage(this.state.getLang);
+    }
+    else if(this.state.getCurrentLang){    
+      CommonActions.changeLanguage(this.state.getCurrentLang);
+    }
+    virtualKeyBoard_login = $('#username, #password').keyboard({
+      layout: 'custom',
+      customLayout: {
+        'default': ['! @ # $ % ^ & * + _', '1 2 3 4 5 6 7 8 9 0 {b}', 'q w e r t y u i o p', 'a s d f g h j k l', '{shift} z x c v b n m . {shift}','{space}', '{a} {c}'],
+        'shift':   ['( ) { } [ ] = ~ ` -', '< > | ? / " : ; , \' {b}', 'Q W E R T Y U I O P', 'A S D F G H J K L', '{shift} Z X C V B N M . {shift}','{space}', '{a} {c}']
+      },
+      css: {
+        container: "ui-widget-content ui-widget ui-corner-all ui-helper-clearfix custom-keypad"
+      },
+      reposition: true,
+      alwaysOpen: false,
+      initialFocus: true,      
+      visible : function(e, keypressed, el){
+        el.value = '';
+        //$(".authNotify").css("display","none"); 
+      },
+      
+      accepted: function(e, keypressed, el) {
+        var usernameValue = document.getElementById('username').value;
+        var passwordValue = document.getElementById('password').value;
+        if(usernameValue != null && usernameValue !=''  && passwordValue != null && passwordValue != '' ){
+          $('#loginBtn').prop('disabled', false);
+        }else{
+          $('#loginBtn').prop('disabled', true); 
+        }    
+      }
+    }); 
+  },
+  componentWillUnmount: function(){
+    mainstore.removeChangeListener(this.onChange);
+    loginstore.removeChangeListener(this.onChange);
+  },
+   componentWillMount:function(){
+    virtualKeyBoard_login = $('#username, #password').keyboard({
+      layout: 'custom',
+      customLayout: {
+        'default': ['! @ # $ % ^ & * + _', '1 2 3 4 5 6 7 8 9 0 {b}', 'q w e r t y u i o p', 'a s d f g h j k l', '{shift} z x c v b n m . {shift}','{space}', '{a} {c}'],
+        'shift':   ['( ) { } [ ] = ~ ` -', '< > | ? / " : ; , \' {b}', 'Q W E R T Y U I O P', 'A S D F G H J K L', '{shift} Z X C V B N M . {shift}','{space}', '{a} {c}']
+      },
+      css: {
+        container: "ui-widget-content ui-widget ui-corner-all ui-helper-clearfix custom-keypad"
+      },
+      reposition: true,
+      alwaysOpen: false,
+      initialFocus: true,      
+      visible : function(e, keypressed, el){
+        el.value = '';
+        //$(".authNotify").css("display","none"); 
+      },
+      
+      accepted: function(e, keypressed, el) {
+        var usernameValue = document.getElementById('username').value;
+        var passwordValue = document.getElementById('password').value;
+        if(usernameValue != null && usernameValue !=''  && passwordValue != null && passwordValue != '' ){
+          $('#loginBtn').prop('disabled', false);
+        }else{
+          $('#loginBtn').prop('disabled', true); 
+        }    
+      }
+    });
+   },
+  onChange: function(){    
+    this.setState(getState());
+
+  },
+  disableLoginButton:function(){
+      $('#loginBtn').prop('disabled', true);
+  },
+  changeLanguage : function(){
+    CommonActions.changeLanguage(this.refs.language.value);
+    this.disableLoginButton();    
+  },
+  removeNotify:function(){
+       $('.errorNotify').css('display','none');
+  },
+  render: function(){
+    var _languageDropDown=(
+      <select className="selectLang"  value={this.state.getCurrentLang} ref='language' onChange={this.changeLanguage} >
+          <option value="en-US">{"English"}</option>
+          <option value="ja-JP">{"日本語"}</option>
+          <option value="de-DE">{"Deutsche"}</option>
+          <option value="zh-ZH">{"中文"}</option>
+          <option value="fr-FR">{"Français"}</option>
+          <option value="es-ES">{"Español"}</option>
+          <option value="nl">{"Dutch"}</option>
+      </select>    
+);
+var  ppsDropDown=(
+  <select className="selectPPS" value={this.state.getCurrentLang} ref='pps' onChange={this.changeLanguage} >
+      <option value="pps1front">{"PPS 1 Front"}</option>
+      <option value="pps2back">{"PPS 2 Back"}</option>
+  </select>
+);
+    return(
+    <div className="containerLogin">
+        <header className="heading">
+        <div className="logo">
+        <img className="imgLogo" src={allSvgConstants.logo} />
+        </div>
+        <div className="languageDropDown">
+          <span className="langText">{appConstants.LANGUAGE}</span>
+        {this.state.getLang?'':_languageDropDown}
+        </div>
+        </header>
+        <div className="subHeading">
+        <div className="langText">{appConstants.LOGINTEXT}</div>
+        {ppsDropDown}
+        </div>
+        <main>
+        <div className="keyboardLogin"></div>
+        <div className="scanIdLogin"></div>
+        </main>
+        <footer>
+        Copyright © 2018 GreyOrange Pte Ltd.
+        </footer>
+  </div>
+    )
+  }
+});
+
+module.exports = LoginPageRevamp;
+

59992a89a35f951c3dc9b0acd50bd432182bdb92
anil kumar <anil.k@greyorange.sg>
2018-10-12 11:40:37 +0000

    BSS-9420: Tote icon should be displayed in bin display on put items

--- src/assets/js/components/CurrentBin.js
+++ src/assets/js/components/CurrentBin.js
@@ -4,10 +4,10 @@ var allresourceConstants = require('../constants/resourceConstants');
 
 var CurrentBin = React.createClass({
     render:function(){  
-             
         return (
                 <div className="current-bin-wrapper">
                 <div className={"p-put-details current-bin "+(this.props.selected ? "selected":"")}>
+                    <div className="tote"><span style={{"top":"6px"}} className={this.props.details.count ? "bin-icon tote-icon": ""}></span></div>
                     <div className="item-count-wrap">
                     <p className="item-count">{this.props.details.count || "--"}</p>
                     <p className="item-count-text">{_("Items in Bin")}</p>

c96ca1f3e9b60d5fb2f6c8af310d119a2e6bd9cc
Anil Kumar <anil.k@GGN000046.local>
2018-11-22 12:57:16 +0530

    added missing icons

--- src/assets/js/components/LoginPage/LoginPage.js
+++ src/assets/js/components/LoginPage/LoginPage.js
@@ -256,7 +256,7 @@ var LoginPage = React.createClass({
         <div className="rightBelow"></div>
         <div className="leftBelow"></div>
         <div className="scanLogo"></div>
-        <span> Scan ID card to login.</span>
+        <div> Scan ID card to login.</div>
         
         
         </div>

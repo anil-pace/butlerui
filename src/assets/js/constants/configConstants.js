cd81a9aea75db3da5e15f672caf44452b5f58392
Anil Kumar <anil.k@GGN000046.local>
2019-01-03 12:29:35 +0530

    testing in progress

--- src/assets/js/constants/configConstants.js
+++ src/assets/js/constants/configConstants.js
@@ -1,6 +1,6 @@
 var configConstants = {
-  WEBSOCKET_IP : "wss://localhost/wss",
-  INTERFACE_IP : "https://localhost"
+  WEBSOCKET_IP : "wss://139.162.60.209/wss",
+  INTERFACE_IP : "https://139.162.60.209"
   };
 var BOI_CONFIG= configConstants.INTERFACE_IP+"/api/components/get_boi_config";
 configConstants["BOI_CONFIG"] = BOI_CONFIG;

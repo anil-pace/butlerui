2454d368f226a8aa7270ac318b5df299af51989a
Anil Kumar <anil.k@GGN000046.local>
2019-05-07 18:28:38 +0530

    reverted to localhost

--- src/assets/js/constants/configConstants.js
+++ src/assets/js/constants/configConstants.js
@@ -1,6 +1,6 @@
 var configConstants = {
-   WEBSOCKET_IP: "ws://192.168.12.235:8888/ws",
-   INTERFACE_IP: "https://192.168.12.235:5000"
+   WEBSOCKET_IP: 'wss://localhost/wss',
+   INTERFACE_IP: 'https://localhost'
 };
 var BOI_CONFIG =
    configConstants.INTERFACE_IP + '/api/components/get_boi_config';

98be64b4dbfbfec03c7d3aadb12e359b5c1c2a64
Sudivya Thakkar <sudivya.t@greyorange.com>
2019-07-08 06:49:42 +0000

    BSS-15083 UI - ARA Picking Enhancements (Mobile)

--- src/assets/js/components/Header.js
+++ src/assets/js/components/Header.js
@@ -206,7 +206,7 @@ var Header = React.createClass({
         return (<div>
             <div className="head">
                 <div className="logo">
-                    <img src={allSvgConstants.logo} style={{ 'width': '142px' }} />
+                    <img src={allSvgConstants.logo}/>
                 </div>
                 <div className="ppsMode"> PPS Mode : {this.state.ppsMode.toUpperCase()} </div>
                 <div className={cssClass} onClick={this.openKeyboard}>

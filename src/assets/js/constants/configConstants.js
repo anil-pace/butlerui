feb1a84bb30b0758a3ae2ab974fb89de77339ade
Anil Kumar (Software Development) <anil.k@greyorange.com>
2020-04-03 04:26:14 +0000

    BSS-22161: PPTL Mgmt...update interface endpoints to core endpoints.

--- src/assets/js/constants/configConstants.js
+++ src/assets/js/constants/configConstants.js
@@ -1,7 +1,6 @@
 var configConstants = {
   WEBSOCKET_IP: "wss://localhost/wss",
-  INTERFACE_IP: "https://localhost",
-  CORE_IP: "https://localhost"
+  INTERFACE_IP: "https://localhost"
 }
 var BOI_CONFIG = configConstants.INTERFACE_IP + "/api/components/get_boi_config"
 configConstants["BOI_CONFIG"] = BOI_CONFIG

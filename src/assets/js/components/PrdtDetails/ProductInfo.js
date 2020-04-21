ef4071bc4247eb3984139b04eaf340b4e79da4fc
Arvind Sangwan (Delivery) <arvind.s@greyorange.com>
2020-04-21 07:45:23 +0000

    BSS-23116: The product info box shows a double image symbol at the pick front screen.

--- src/assets/js/components/PrdtDetails/ProductInfo.js
+++ src/assets/js/components/PrdtDetails/ProductInfo.js
@@ -4,7 +4,7 @@ var ProductImage = require("../PrdtDetails/ProductImage")
 var ProductInfo = React.createClass({
     render: function() {
         var infoDetails = this.props.infoDetails;
-        var imageurl = this.props.imageurl;
+        var imageurl = this.props.hasOwnProperty('imageurl') ? this.props.imageurl : '-';
         var PutContainerFlag = this.props.putContainerFlag;
         var arr1 = [];
         $.each(infoDetails, function(key, value) {
@@ -33,9 +33,10 @@ var ProductInfo = React.createClass({
             <div className="packingBoxTableInfo" style={{width: "480px",
                 height: "549px",
                 marginLeft: "18%", display:"block"}}>
+                    {imageurl !== '-' ? 
             <div className="packingBoxImage">
             <ProductImage srcURL={imageurl} />
-            </div>
+            </div> : ''}
             {arr1}
         </div>
         );

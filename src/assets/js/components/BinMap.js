b4e15a8b5d509019d1b741da1116edcb254525db
Arvind Sangwan <GREYORANGE\arvind.s@ggn001273.greyorange.com>
2020-04-30 18:32:15 +0530

    BSS-23367:  code checkins for binmap Component  Nav Box

--- src/assets/js/components/BinMap.js
+++ src/assets/js/components/BinMap.js
@@ -20,11 +20,11 @@ var BinMap = React.createClass({
 			}}}
 
 		maxBlockCount = maxLeftCount > maxRightCount ? maxLeftCount :maxRightCount;
-		maxBlockHeight = 40/maxBlockCount;
+		maxBlockHeight = maxBlockCount !== 0  ? 40/maxBlockCount : 0;
 		maxWidth = ((maxBlockHeight/100)*150);
 		style = {
 			height:maxBlockHeight+"%",
-			width: maxWidth <= 38 ? maxWidth : 38
+			width: maxWidth <= 38 ? 38 : maxWidth
 		}
 		if(Object.entries(bindata).length > 0 ){
 		for(let i=0; i<bindata.length; i++){
@@ -36,6 +36,14 @@ var BinMap = React.createClass({
 				}
 		}
 	}
+
+	if(leftCol.length === 0){
+		leftCol.push(<li  style={{height: "105px"}} className ={'col1Zero'}></li>);
+	}
+	if(rightCol.length === 0){
+		rightCol.push(<li style={{height: "105px"}} className={'col3Zero'} ></li>);
+	}
+
 		switch(leftCol.length){
 			case 1:
 			leftColCount = "one";

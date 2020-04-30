747ffd7c5836fd7696bb18532f86b39bf27d1282
Arvind Sangwan <GREYORANGE\arvind.s@ggn001273.greyorange.com>
2020-04-30 18:19:34 +0530

    BSS-23367: Binmap generic fix

--- src/assets/js/components/BinMap.js
+++ src/assets/js/components/BinMap.js
@@ -20,7 +20,7 @@ var BinMap = React.createClass({
 			}}}
 
 		maxBlockCount = maxLeftCount > maxRightCount ? maxLeftCount :maxRightCount;
-		maxBlockHeight = 40/maxBlockCount;
+		maxBlockHeight = maxBlockCount !== 0  ? 40/maxBlockCount : 0;
 		maxWidth = ((maxBlockHeight/100)*150);
 
 		style = {
@@ -37,13 +37,13 @@ var BinMap = React.createClass({
 					rightCol.push(<li key={k} style={style} className={'sel'}></li>);
 				}
 		}
+	}
 
-		if(leftCol.length === 0){
-			leftCol.push(<li  style={{height: "105px"}} className ={'col1Zero'}></li>);
-		}
-		else if(rightCol.length === 0){
-			rightCol.push(<li style={{height: "105px"}} className={'col3Zero'} ></li>);
-		}
+	if(leftCol.length === 0){
+		leftCol.push(<li  style={{height: "105px"}} className ={'col1Zero'}></li>);
+	}
+	if(rightCol.length === 0){
+		rightCol.push(<li style={{height: "105px"}} className={'col3Zero'} ></li>);
 	}
 		switch(leftCol.length){
 			case 1:

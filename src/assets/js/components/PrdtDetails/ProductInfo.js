89df3109d5003cfcf3a0a7ef27509f5c972d2c04
Sudivya Thakkar <sudivya.t@greyorange.com>
2020-05-07 17:36:16 +0530

    BSS-23689: [UDP flow] - No data is coming in the details section box of product when an item is scan at put fornt screen.

--- src/assets/js/components/PrdtDetails/ProductInfo.js
+++ src/assets/js/components/PrdtDetails/ProductInfo.js
@@ -1,68 +1,72 @@
-var React = require('react');
+var React = require("react")
 var ProductImage = require("../PrdtDetails/ProductImage")
 
 var ProductInfo = React.createClass({
-    render: function() {
-        var infoDetails = this.props.infoDetails;
-        let flowIndicator = this.props.flowIndicator;
-        var imageurl = this.props.hasOwnProperty('imageurl') ? this.props.imageurl : '-';
-        var PutContainerFlag = this.props.putContainerFlag;
-        var arr1 = [];
+  render: function () {
+    var infoDetails = this.props.infoDetails
+    let flowIndicator = this.props.flowIndicator
+    var imageurl = this.props.hasOwnProperty("imageurl")
+      ? this.props.imageurl
+      : "-"
+    var PutContainerFlag = this.props.putContainerFlag
+    var arr1 = []
 
-        flowIndicator === "Pick" ?
+    flowIndicator === "Pick"
+      ? $.each(infoDetails, function (key, value) {
+          return arr1.push(
+            <tr>
+              <td className="key"> {key} </td>
+              <td className="value">{value} </td>
+            </tr>
+          )
+          return arr1
+        })
+      : $.each(infoDetails, function (key, value) {
+          if (
+            key === "QlcodeDigits" &&
+            value !== "" &&
+            PutContainerFlag === true
+          ) {
+            arr1.push(
+              <div className="detailsOuterWrapper">
+                <span className="detailsDispValShort">{value}</span>
+              </div>
+            )
+          } else if (key !== "QlcodeDigits") {
+            arr1.push(
+              <div className="detailsOuterWrapper">
+                <div className="detailsInnerWrapper">
+                  <span className="detailsDispName"> {key + ":"} </span>
+                  <span className="detailsDispVal">{value}</span>
+                </div>
+              </div>
+            )
+          }
+          return arr1
+        })
 
-$.each(infoDetails, function(key, value) {
-            return arr1.push(
-                                <tr>
-                	  				<td className="key"> {key} </td>
-                	  				<td className="value">{value} </td>
-                  				</tr>                         
-            );   
-            return  arr1
-        }) :
-
-        $.each(infoDetails, function(key, value) {
-            if(key === "QlcodeDigits" && value !== '' && PutContainerFlag === true){
-                arr1.push(
-                    <div className="detailsOuterWrapper">
-                     <span className="detailsDispValShort">{value}</span> 
-                     </div>
-                     );
-            }
-            else if(key !== 'QlcodeDigits'){
-                arr1.push(
-                    <div className="detailsOuterWrapper">
-                    <div className="detailsInnerWrapper">
-                     <span className="detailsDispName"> {key + ':'} </span>
-                      <span className="detailsDispVal">{value}</span>
-                    </div>
-                    </div>
-    
-                );
-            }   
-            return  arr1
-        });
-
-        return (
-            flowIndicator === "Pick" ? 
-            <div className="table-wrapper">
-				<table className="table">									
-					<tbody>
-						{arr1}
-					</tbody>
-				</table>
-			</div> : 
-            <div className="packingBoxTableInfo" style={{width: "520px",
-                height: "549px",
-                marginLeft: "18%", display:"block"}}>
-                    {imageurl !== '-' ? 
-            <div className="packingBoxImage">
+    return flowIndicator === "Pick" ? (
+      <div className="table-wrapper">
+        <table className="table">
+          <tbody>{arr1}</tbody>
+        </table>
+      </div>
+    ) : (
+      <div
+        className="packingBoxTableInfo"
+        style={{ width: "520px", height: "549px", display: "block" }}
+      >
+        {imageurl !== "-" ? (
+          <div className="packingBoxImage">
             <ProductImage srcURL={imageurl} />
-            </div> : ''}
-            {arr1}
-        </div>
-        );
-    }
-});
+          </div>
+        ) : (
+          ""
+        )}
+        {arr1}
+      </div>
+    )
+  },
+})
 
-module.exports = ProductInfo;
+module.exports = ProductInfo

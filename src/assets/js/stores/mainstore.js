65e3ee19e7aab5fd0f6a580e1d5f9814639bbc9b
Anil Kumar (Software Development) <anil.k@greyorange.com>
2020-06-01 10:26:59 +0000

    BSS-22854: Sanitise Supervision tree (Sidelining of bins)(PFI PArt)

--- src/assets/js/stores/mainstore.js
+++ src/assets/js/stores/mainstore.js
@@ -1637,6 +1637,21 @@ var mainstore = objectAssign({}, EventEmitter.prototype, {
       return _seatData.ops_paused
     } else return null
   },
+  getSystemAuditError: function () {
+    if (_seatData.hasOwnProperty("audit_sideline_popup")) {
+      return _seatData.audit_sideline_popup
+    } else return null
+  },
+  getBinToSideline: function () {
+    if (_seatData.hasOwnProperty("bin_to_sideline")) {
+      return _seatData.bin_to_sideline
+    } else return []
+  },
+  getSystemPickError: function () {
+    if (_seatData.hasOwnProperty("auto_sideline_confirm_required")) {
+      return _seatData.auto_sideline_confirm_required
+    } else return null
+  },
   getGamificationExpand: function () {
     if (_seatData.hasOwnProperty("grafana_main_url")) {
       return _seatData.grafana_main_url

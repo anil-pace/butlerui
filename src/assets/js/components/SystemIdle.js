70cb2b08c60ccece1156de4468058c8076a557cc
Anil kumar <anil.k@greyorange.com>
2020-06-03 15:26:41 +0530

    BSS-23567: new screen introduced as per hld

--- src/assets/js/components/SystemIdle.js
+++ src/assets/js/components/SystemIdle.js
@@ -6,13 +6,29 @@ var mainstore = require('../stores/mainstore');
 
 var SystemIdle = React.createClass({
 	render:function(){
+		var isAllPpsBlocked = mainstore.getAllPpsBlocked();
 		var footer = mainstore.getGamificationUrl() ? <Footer /> : ""
 		return (
 				<div className="systemIdle">
 					<Header />
-					<div className="idleScreen">
-						{_(allresourceConstants.SYS_IDLE)}
-					</div>
+					{isAllPpsBlocked ? 
+						(<div className="idleScreen">
+							<div className="ppsBlocked">
+							<span>{_("All the bins are")} </span>
+							<span>{_("currently facing")} </span>
+							<br></br>
+							<span>{_("some issue.")} </span>
+							<br></br>
+							<span>{_("Please contact support.")} </span>
+							</div>
+						</div>) 
+						:
+						(
+						<div className="idleScreen">
+							{_(allresourceConstants.SYS_IDLE)}
+						</div>
+						)
+					}
 					{footer}
 				</div>
 			);
